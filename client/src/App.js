518137cf23ea0482172a8ab065cf35cfc5c86ac7
Richard Almonte <richard.almonte@bridgesite.io>
2018-11-13 18:59:59 -0500

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+import LogInPage from './components/LogInPage'
 
+const Global = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+  background: #A8DADC;
+}
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Global />
+          {/* <NavBar /> */}
+          <Switch>
+            {/* <Route exact path="/login" component={LogInPage}/> */}
+            {/* <Route exact path="/users/:userId" component={IdeaPage}/> */}
+            <Route path="/" component={LogInPage}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
