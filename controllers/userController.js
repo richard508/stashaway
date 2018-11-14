b67dcfbad4909f751c228e00fc15a8dbe8c1b55f
Richard Almonte <richard.almonte@bridgesite.io>
2018-11-13 19:00:42 -0500

    format userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,37 +2,37 @@ const User = require('../models/User')
 const Stash = require('../models/Stash')
 
 const userController = {
-    index: (req, res) => {
-        User.find({})
-            .then((users) => {
-                res.send(users)
-            })
-    },
-    show: (req, res) => {
-        User.findById(req.params.userId).populate('stashes')
-            .then((user) => {
-                res.send(user)
-            })
-    },
-    update: (req, res) => {
-        User.findByIdAndUpdate(req.params.userId, req.body)
-            .then((updatedUser) => {
-                updatedUser.save()
-                res.send(updatedUser)
-            })
-    },
-    delete: (req, res) => {
-        User.findByIdAndDelete(req.params.userId)
-            .then(() => {
-                res.send(200)
-            })
-    },
-    create: (req, res) => {
-        User.create(req.body)
-            .then((user) => {
-                res.send(user)
-            })
-    }
+  index: (req, res) => {
+    User.find({})
+      .then((users) => {
+          res.send(users)
+      })
+  },
+  show: (req, res) => {
+    User.findById(req.params.userId).populate('stashes')
+      .then((user) => {
+          res.send(user)
+      })
+  },
+  update: (req, res) => {
+    User.findByIdAndUpdate(req.params.userId, req.body)
+      .then((updatedUser) => {
+          updatedUser.save()
+          res.send(updatedUser)
+      })
+  },
+  delete: (req, res) => {
+    User.findByIdAndDelete(req.params.userId)
+      .then(() => {
+          res.send(200)
+      })
+  },
+  create: (req, res) => {
+    User.create(req.body)
+      .then((user) => {
+          res.send(user)
+      })
+  }
 }
 
 module.exports = userController
